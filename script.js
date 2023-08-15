52674ce221879692fb309f0d48d52e9e3237cd0c
joaovitorangelo <joaovitorangelo05@gmail.com>
2023-08-15 13:37:57 -0300

    acebandmot

---
+++ script.js
@@ -0,0 +1,7 @@
+//NAVBAR
+let navigation = document.querySelector('.navigation');
+let toggle = document.querySelector('.toggle');
+toggle.onclick = function(){
+    navigation.classList.toggle('active')
+}
+//
