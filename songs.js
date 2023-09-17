552ea6f61c9d6f537c7db564d285a2cd93bb69a0
joaovitorangelo <joaovitorangelo05@gmail.com>
2023-09-16 21:17:36 -0300

    add aceband radio

---
+++ songs.js
@@ -0,0 +1,55 @@
+export default [
+    {
+      src: "./src/lesoes-cervicais.mp3",
+      name: "mist - lesoes cervicais",
+    },
+    {
+      src: "./src/brisa-de-outono.mp3",
+      name: "mist - brisa de outono",
+    },
+    {
+      src: "./src/she.mp3",
+      name: "mist - she",
+    },
+    {
+      src: "./src/ruido.mp3",
+      name: "mist - ruido",
+    },
+    {
+      src: "./src/story_of_a_artist.mp3",
+      name: "mist - story of a artist",
+    },
+    {
+      src: "./src/palhacos-do-blues.mp3",
+      name: "eminem - palhacos do blues",
+    },
+    {
+      src: "./src/don't-leave-me.mp3",
+      name: "poletto - don't leave me",
+    },
+    {
+      src: "./src/and_i_love_her.mp3",
+      name: "haruki - and i love her",
+    },
+    {
+      src: "./src/cassiopeia.mp3",
+      name: "haruki - cassiopeia",
+    },
+    {
+      src: "./src/ceu-estrelado.mp3",
+      name: "haruki - ceu estrelado",
+    },
+    {
+      src: "./src/amago.mp3",
+      name: "haruki - âmago",
+    },
+    {
+      src: "./src/little-gray-cat.mp3",
+      name: "haruki - little gray cat",
+    },
+    {
+      src: "./src/marido-da-esposa-viuva.mp3",
+      name: "haruki - marido da esposa viuva",
+    },
+  ];
+  
